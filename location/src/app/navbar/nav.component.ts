166b1da4eb941b713c717a22b54b2eff1648b0b6
OussemaDrira <oussemadrira1@gmail.com>
2024-06-14 14:27:58 +0100

    first commit

---
+++ location/src/app/navbar/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+  userEmail: string | null = '';
+
+  ngOnInit() {
+    this.userEmail = localStorage.getItem('userEmail');
+  }
+  logout(): void {
+    // For example, clearing local storage, removing cookies, etc.
+    localStorage.removeItem('userEmail'); // Remove userEmail from localStorage
+    this.userEmail = null;
+    window.location.reload();
+  }
+}
